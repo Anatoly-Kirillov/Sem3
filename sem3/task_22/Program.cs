fa46085eadc39b81e340bc921dd35c103dd6d5a3
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-25 21:34:13 +0300

    task_22

---
+++ sem3/task_22/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine());
+int count = 1;
+while(count <= numberN)
+{
+    Console.Write($"{count*count}, ");
+    count++;
+}
+Console.WriteLine();
+for (int i=1; i<=numberN; i++)
+{
+    Console.Write($"{i*i}, ");
+}
+Console.WriteLine();
