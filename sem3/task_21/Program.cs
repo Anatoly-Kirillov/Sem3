9207e1f34119612bb9cdcbb3b171d26c47c77a14
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-25 20:59:27 +0300

    Нахождение расстояния между точками на оси координат

---
+++ sem3/task_21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите x1");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y1");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите x2");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y2");
+int y2 = int.Parse(Console.ReadLine());
+
+int sqr1 = (x1 - x2) * (x1 - x2);
+int sqr2 = (y1 - y2) * (y1 - y2);
+
+int sum = sqr1 + sqr2;
+
+double result = Math.Sqrt(sum);
+
+Console.WriteLine($"{result:f2}");
