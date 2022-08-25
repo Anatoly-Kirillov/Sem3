f9a8ad1074bba07cd2f2c0c5a459410236526ea8
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-25 20:30:02 +0300

    Ось координат по четверти

---
+++ sem3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите номер четверти 1-4: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("х > 0 и  y >0");
+}
+else
+if (num == 2)
+{
+    Console.WriteLine("х < 0 и  y >0");
+}
+else
+if (num == 3)
+{
+    Console.WriteLine("х < 0 и  y <0");
+}
+else 
+if (num == 4)
+{
+    Console.WriteLine("х > 0 и  y <0");
+}
+else
+{
+    Console.WriteLine("Введите корректный номер четверти от 1 до 4");
+}
